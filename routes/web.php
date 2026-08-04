f8a26e3c6e02789792a5bca435a919d3ec23ccca
Sébastien Roger <sebastien.roger@gmail.com>
2026-08-04 18:11:15 +0200

    Sprint 5 - Installation de Laravel Breeze et authentification

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])
-    ->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/reservation/{eventSession}', [ReservationController::class, 'create'])
-    ->name('reservations.create');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/reservation/{eventSession}', [ReservationController::class, 'store'])
-    ->name('reservations.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/admin', [DashboardController::class, 'index'])
-    ->name('admin.dashboard');
+require __DIR__.'/auth.php';
